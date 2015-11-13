dbaf27090350a82f9b5c3162a84affa22f29e0be
ihtfw <vkandrushin@gmail.com>
2015-11-13 14:21:52 +0600

    Created Integration lib for easy use from other apps

---
+++ nuget/build-appveyor.ps1
@@ -0,0 +1,23 @@
+$projDirName = "Logazmic.Integration"
+$projName = "Logazmic.Integration"
+
+$curDir = (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+$slnDir = $curDir + "\..\src\"
+$projDir = $slnDir + $projDirName+ "\"
+
+$version = (Get-Date -format yyyy.M.d) + "." + $env:appveyor_build_number;
+
+$nuspecPath = ($curDir + "\" + $projName + ".nuspec")
+$nupkgPath = ($curDir + "\" + $projName + "." + $version + ".nupkg")
+
+Set-Location $curDir
+
+Write-Host "------------------Setting nuspec version($vesrsion)------------------"
+[xml]$nuspec = Get-Content $nuspecPath
+$nuspec.package.metadata.version = $version
+$nuspec.Save($nuspecPath)
+
+Write-Host "------------------Create nupkg------------------"
+& ($curDir + "\bin\nuget.exe") pack $nuspecPath -outputdirectory ($curDir + "\Releases")
+
+
---
+++ src/Logazmic.Integration/InstallationChecker.cs
@@ -0,0 +1,52 @@
+﻿namespace Logazmic.Integration
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    public class InstallationChecker
+    {
+        public bool IsInstalled()
+        {
+            if (!Directory.Exists(LogazmicDir))
+            {
+                return false;
+            }
+
+            if (!Directory.Exists(Path.Combine(LogazmicDir, "packages")))
+            {
+                return false;
+            }
+
+            if (File.Exists(Path.Combine(LogazmicDir, ".dead")))
+            {
+                return false;
+            }
+
+            if (!File.Exists(UpdatePath))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string LogazmicDir => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Logazmic");
+
+        public string UpdatePath => Path.Combine(LogazmicDir, "Update.exe");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathToSetup"></param> 
+        /// <exception cref="LogazmicIntegrationException"></exception>
+        public void Install(string pathToSetup)
+        {
+            if (!File.Exists(pathToSetup))
+                throw new LogazmicIntegrationException("Setup file not found");
+
+            var process = Process.Start(pathToSetup);
+            process.WaitForExit();
+        }
+    }
+}
