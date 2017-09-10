decf0bd6f67f8e15be35787d0dea8218b826390f
Mikhail Kanygin <kanygin@vector-best.ru>
2017-09-11 00:28:39 +0700

    NLogSender test program now has command line options for more flexible testing.

--- src/NLogSender/Program.cs
+++ src/NLogSender/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NDesk.Options;
 
 namespace NLogSender
 {
@@ -46,18 +47,18 @@ namespace NLogSender
             {
                 for (int i = 0; i < _numberOfHits; i++)
                 {
-                    switch (i % 3)
+                    switch (i % 4)
                     {
                         case 0:
-                            _localLogger.Trace(Messages.HugeMessage);
+                            _localLogger.Trace($"{i}: {Messages.HugeMessage}");
                             break;
 
                         case 1:
-                            _localLogger.Debug(Messages.LargeMessage);
+                            _localLogger.Debug($"{i}: {Messages.LargeMessage}");
                             break;
 
                         case 2:
-                            _localLogger.Error(Messages.ExceptionMessage, new IOException());
+                            _localLogger.Error($"{i}: {Messages.ExceptionMessage}", new IOException());
                             break;
 
                         default:
@@ -67,17 +68,19 @@ namespace NLogSender
                     Thread.Sleep(_hitCooldown);
                 }
 
-                _localLogger.Warn($"{_localLogger.Name} done!");
+                Console.Out.WriteLine($"{_localLogger.Name} done!");
             }
         }
 
-        public static void SetupNLog()
+        static OptionSet _optionSet;
+
+        public static void SetupNLog(int port = 4505)
         {
             var config = new LoggingConfiguration();
             
             var tcpNetworkTarget = new NLogViewerTarget
             {
-                Address = "tcp4://127.0.0.1:4505",
+                Address = $"tcp4://127.0.0.1:{port}",
                 Encoding = Encoding.UTF8,
                 Name = "NLogViewer",
                 IncludeNLogData = false
@@ -91,12 +94,48 @@ namespace NLogSender
 
         static void Main(string[] args)
         {
+            var numberOfHits = 1000;
+            var cooldown = 20;
+            var numberOfThreads = 1;//Environment.ProcessorCount;
+            var port = 4505;
+
+            _optionSet = new OptionSet
+            {
+                {
+                    "t|threads=", $"Number of flood thread. {numberOfThreads} by-default.",
+                    t => { int.TryParse(t, out numberOfThreads); }
+                },
+                {
+                    "m|messages=", $"Number of messages per thread. {numberOfHits} by-default.", h =>
+                    {
+                        int.TryParse(h, out numberOfHits);
+                    }
+                },
+                {
+                    "c|cooldown=", $"Cooldown after each message in miliseconds. {cooldown} by-default", c =>
+                    {
+                        int.TryParse(c, out cooldown);
+                    }
+                },
+                {
+                    "p|port=", $"TCP port. Default is {port}", p =>
+                    {
+                        int.TryParse(p, out port);
+                    }
+                },
+                {
+                    "h|help", "Show help", _ =>
+                    {
+                        _optionSet.WriteOptionDescriptions(Console.Out);
+                        Environment.Exit(0);
+                    }
+                }
+            };
+            _optionSet.Parse(args);
+
             SetupNLog();
 
-            var numberOfHits = 10000;
-            var cooldown = 20;
-
-            var numberOfThreads = Environment.ProcessorCount;
+            Console.Out.WriteLine($"Starting flood with\n\tPort:\t{port}\n\tThreads:\t{numberOfThreads}\n\tMessages per thread:\t{numberOfHits}\n\tCooldown after message:\t{cooldown} milisec");
 
             var flooders = Enumerable.Range(1, numberOfThreads)
                 .Select(_ => new FloodContext(numberOfHits, cooldown))
