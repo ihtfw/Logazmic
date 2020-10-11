62350c8f4d596e5e427174314d96fef9db66c471
Vadim Kandrushin <vkandrushin@gmail.com>
2020-10-11 14:45:38 +0700

    Flat log file supports exact template, that I need

--- src/Logazmic/Core/Readers/Parsers/FlatLogParser.cs
+++ src/Logazmic/Core/Readers/Parsers/FlatLogParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Logazmic.Core.Log;
 
 namespace Logazmic.Core.Readers.Parsers
@@ -8,6 +9,12 @@ namespace Logazmic.Core.Readers.Parsers
     {
         public LogMessage ParseLogEvent(string logEvent)
         {
+            var logMessage = ParseExactTemplateOrDefault(logEvent);
+            if (logMessage != null)
+            {
+                return logMessage;
+            }
+
             return new LogMessage
             {
                 LoggerName = LogFormats.Flat,
@@ -16,6 +23,69 @@ namespace Logazmic.Core.Readers.Parsers
                 TimeStamp = DateTime.Now,
                 LogLevel = LogLevel.Info
             };
+        }
+
+        /// <summary>
+        /// Supported formats:
+        /// 2020-09-03 18:14:10.103 +07:00 [INF] Request finished in 89.4299ms 304
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// <returns></returns>
+        private LogMessage ParseExactTemplateOrDefault(string logEvent)
+        {
+            var indexOfOpenBracket = logEvent.IndexOf(" [", StringComparison.InvariantCultureIgnoreCase);
+            if (indexOfOpenBracket < 1)
+                return null;
+
+            var indexOfCloseBracket = logEvent.IndexOf("] ", indexOfOpenBracket, StringComparison.InvariantCultureIgnoreCase);
+            if (indexOfCloseBracket < 1)
+                return null;
+
+            var logLevelRaw = logEvent.Substring(indexOfOpenBracket + 2, indexOfCloseBracket - indexOfOpenBracket - 2);
+            LogLevel logLevel = TryParseLogLevel(logLevelRaw);
+            if (logLevel == LogLevel.None) return null;
+
+            var timestampRaw = logEvent.Substring(0, indexOfOpenBracket);
+            if (!DateTime.TryParseExact(timestampRaw, "yyyy-MM-dd HH:mm:ss.fff zzz", CultureInfo.InvariantCulture, DateTimeStyles.None, out var timestamp))
+            {
+                return null;
+            }
+
+            return new LogMessage
+            {
+                LoggerName = LogFormats.Flat,
+                ThreadName = "NA",
+                Message = logEvent.Substring(indexOfCloseBracket + 2),
+                TimeStamp = timestamp,
+                LogLevel = logLevel
+            };
+        }
+
+        private static LogLevel TryParseLogLevel(string logEvent)
+        {
+            switch (logEvent.Trim().ToUpperInvariant())
+            {
+                case "TCE":
+                case "TRC":
+                case "TRACE":
+                    return LogLevel.Trace;
+                case "DBG":
+                case "DEBUG":
+                    return LogLevel.Debug;
+                case "INF":
+                case "INFO":
+                case "INFORMATION":
+                    return LogLevel.Info;
+                case "WRN":
+                case "WARN":
+                case "WARNING":
+                    return LogLevel.Warn;
+                case "FTL":
+                case "FATAL":
+                    return LogLevel.Fatal;
+                default:
+                    return LogLevel.None;
+            }
         }
 
         public IEnumerable<LogEventParseItem> SplitToLogEventParseItems(string text)
